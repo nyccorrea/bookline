bb682ddeea9cb0ac58e724ab240d36083bb465b0
Nicoly Correa <tiestudos.nicoly@gmail.com>
2023-11-06 21:37:28 -0400

    diminui um pouco as linhas de códigos

--- src/features/IniciarSistema.java
+++ src/features/IniciarSistema.java
@@ -1,23 +1,14 @@
 package features;
 
-import users.Funcionario;
-import users.Discente;
-
 import java.util.Scanner;
 
 import features.cadastro.CadastrarDisc;
 import features.cadastro.CadastrarFunc;
-import features.cadastro.SalvarDiscente;
-import features.cadastro.SalvarFuncionario;
+
 
 public class IniciarSistema {
     public static void main(String[] args) {
-        //construtores das classes 
-        Login login = new Login();
-        Funcionario funcionario = new Funcionario();
-        CadastrarFunc func = new CadastrarFunc();
-        Discente discente = new Discente();
-        CadastrarDisc disc = new CadastrarDisc();
+
         //inicia o sistema
         Scanner scanner = new Scanner(System.in);
         System.out.println("------------------ BEM-VINDO AO BOOKLINE ------------------");
@@ -26,16 +17,7 @@ public class IniciarSistema {
         scanner.nextLine(); // Consumir a quebra de linha
 
         if(resposta == 1){
-            //bloco que pega os inputs para usar na entrada do sistema
-            System.out.println("--------- LOGIN ---------");
-            System.out.print("Email: ");
-            login.email_informado = scanner.nextLine();
-            System.out.print("Senha: ");
-            login.senha_informada = scanner.nextLine();
-
-            //valida email e senha
-            login.realizarLogin(discente.getEmail_disc(), discente.getSenha_disc(), funcionario.getEmail_func(), funcionario.getSenha_func());
-           // mostra livros
+            Login.main(args);
             Catalogo.main(args);
         }else if(resposta == 2){
             //bloco que pega os inputs para usar no cadastro do sistema
@@ -44,41 +26,10 @@ public class IniciarSistema {
             char opcao = scanner.next().toLowerCase().charAt(0);
 
             if(opcao == 'f'){
-                System.out.println("Por favor funcionário, informe os dados abaixo.");
-                System.out.print("Nome: ");
-                String nome = scanner.next();
-                System.out.print("Matrícula: ");
-                String matricula = scanner.next();
-                System.out.print("CPF: ");
-                String cpf = scanner.next();
-                System.out.print("Email: ");
-                String email = scanner.next();
-                System.out.print("Crie uma senha: ");
-                String senha = scanner.next();
-
-                //realiza o cadastro do funcionario
-                func.realizarCadastroFunc(funcionario, nome, matricula, cpf, email, senha);
-                //armazena os dados do funcionário para o arquivo "funcionario.txt"
-                SalvarFuncionario.salvarDadosFuncionario(funcionario);
+                CadastrarFunc.main(args);
                 
             }else if(opcao == 'd'){
-                System.out.println("Por favor discente, informe os dados abaixo.");
-                System.out.print("Nome: ");
-                String nome = scanner.next();
-                System.out.print("Matrícula: ");
-                String matricula = scanner.next();
-                System.out.print("CPF: ");
-                String cpf = scanner.next();
-                System.out.print("Email: ");
-                String email = scanner.next();
-                System.out.print("Crie uma senha: ");
-                String senha = scanner.next();
-
-                //realiza o cadastro do discente
-                disc.realizarCadastroDisc(discente, nome, matricula, cpf, email, senha);
-                //armazena os dados do discente para o arquivo "discente.txt"
-                SalvarDiscente.salvarDadosDiscente(discente);
-                Catalogo.main(args);
+                CadastrarDisc.main(args);
             }else{
                 System.out.println("Valor inválido!");
             }
